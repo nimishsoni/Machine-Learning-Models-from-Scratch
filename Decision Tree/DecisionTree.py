23f31b40b10f5494e057944ba65dbfc66fd5c8b7
Nimish <73768660+nimishsoni@users.noreply.github.com>
2024-01-21 21:26:47 +0530

    Decision Tree Modified Information Gain function

--- Decision Tree/DecisionTree.py
+++ Decision Tree/DecisionTree.py
@@ -1,3 +1,5 @@
+# The code implements Decision Tree from scratch
+
 import numpy as np
 from collections import Counter
 # Define Node Class representing internal and Leaf nodes
@@ -5,13 +7,13 @@ from collections import Counter
 class Node:
     def __init__(self, feature = None, threshold=None, left=None, right=None,*,value=None):
         self.feature = feature# feature used for split, none for leaf node
-        self.threshold = threshold, # Threhold of feature used for split, none for leaf node
+        self.threshold = threshold # Threhold of feature used for split, none for leaf node
         self.left = left # left child node, none for leaf node
         self.right = right #right child none, none for leaf node
         self.value=None # Predicted output or class for leaf node, none for internal node
 
-def is_leaf_node(self):
-    return self.value is not None #Returns True if value is non None in case of leaf node
+    def is_leaf_node(self):
+        return self.value is not None #Returns True if value is non None in case of leaf node
 
 class DecisionTree:
     def __init__(self, min_samples_split=2, max_depth = 100, n_features=None):
@@ -37,12 +39,15 @@ class DecisionTree:
         if (depth>=self.max_depth or n_labels==1 or n_samples<self.min_samples_split):
             leaf_value = self._most_common_label(y) # compute prediction for the leaf
             return Node(value=leaf_value)
+        
+            # check if best_thresh is None
 
         # Randomly selects a subset of features (self.n_features)  without replacement.
         feat_idxs = np.random.choice(n_feats, self.n_features, replace=False)
         
         # find the best split - Best feature and Best threshold
         best_feature, best_thresh = self._best_split(X, y, feat_idxs)
+
 
         # create child nodes
         # Splits the data into left and right indices based on the best feature and threshold.
@@ -81,31 +86,35 @@ class DecisionTree:
 
         return split_idx, split_threshold
 
-    def _information_gain(self, y, X_column, threshold):
+    def _information_gain(self, y, X_column, threshold): # Calculates information gain for potential split based on feature
         # parent entropy
-        parent_entropy = self._entropy(y)
+        parent_entropy = self._entropy(y) 
 
-        # create children
+        # create children using feature column data from node based on threshold
         left_idxs, right_idxs = self._split(X_column, threshold)
 
         if len(left_idxs) == 0 or len(right_idxs) == 0:
             return 0
         
         # calculate the weighted avg. entropy of children
-        n = len(y)
-        n_l, n_r = len(left_idxs), len(right_idxs)
-        e_l, e_r = self._entropy(y[left_idxs]), self._entropy(y[right_idxs])
-        child_entropy = (n_l/n) * e_l + (n_r/n) * e_r
-
+        n = len(y) # Length of parent node
+        n_l, n_r = len(left_idxs), len(right_idxs) # number of data sample in left and right child nodes
+        e_l, e_r = self._entropy(y[left_idxs]), self._entropy(y[right_idxs]) # Entropy of left and right child nodes
+        child_entropy = (n_l/n) * e_l + (n_r/n) * e_r # Weighted average of entropy based on number of samples in child node
+        
+        # calculate the IG
+        information_gain = parent_entropy - child_entropy
+        return information_gain
+    
     def _split(self, X_column, split_thresh):
         left_idxs = np.argwhere(X_column <= split_thresh).flatten()
         right_idxs = np.argwhere(X_column > split_thresh).flatten()
         return left_idxs, right_idxs
 
     def _entropy(self, y):
-        hist = np.bincount(y)
-        ps = hist / len(y)
-        return -np.sum([p * np.log(p) for p in ps if p>0])
+        hist = np.bincount(y) # Generates a histogram of class labels.
+        ps = hist / len(y) # Compute probability of each label
+        return -np.sum([p * np.log(p) for p in ps if p>0]) #Compute entropy
 
 
     def _most_common_label(self, y):
@@ -113,7 +122,7 @@ class DecisionTree:
         value = counter.most_common(1)[0][0]
         return value
 
-    def predict(self, X):
+    def predict(self, X): 
         return np.array([self._traverse_tree(x, self.root) for x in X])
 
     def _traverse_tree(self, x, node):
